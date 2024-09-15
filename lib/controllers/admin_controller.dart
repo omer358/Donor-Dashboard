a51244bad270091fd77e9f66f3e64bdd2d8fbd71
omer358 <omermaki358@gmail.com>
2024-09-15 04:50:39 +0300

    fix the navigation rail selectedIndex

--- lib/controllers/admin_controller.dart
+++ lib/controllers/admin_controller.dart
@@ -1,9 +1,13 @@
+import 'dart:developer';
+
 import 'package:get/get.dart';
 
 class AdminController extends GetxController {
   var selectedPage = 0.obs;
 
   void changePage(int index) {
+    log("the current index is $index");
     selectedPage.value = index;
+    log("the new selectedPage is ${selectedPage.value}");
   }
 }
--- lib/views/admin_dashboard_screen.dart
+++ lib/views/admin_dashboard_screen.dart
@@ -18,58 +18,61 @@ class AdminDashboardScreen extends StatefulWidget {
 class _AdminDashboardScreenState extends State<AdminDashboardScreen> {
   final AdminController adminController = Get.put(AdminController());
 
-  final DonationRequestsController donationRequests = Get.put(DonationRequestsController());
+  final DonationRequestsController donationRequests = Get.put(
+      DonationRequestsController());
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Directionality(
         textDirection: TextDirection.rtl,
-        child: Row(
-          children: [
-            NavigationRail(
-              selectedIndex: adminController.selectedPage.value,
-              onDestinationSelected: adminController.changePage,
-              labelType: NavigationRailLabelType.selected,
-              elevation: 4,
-              trailing: IconButton(
-                icon: const Icon(Icons.logout)
-                , onPressed: () {
+        child: Obx(() {
+          return Row(
+            children: [
+              NavigationRail(
+                selectedIndex: adminController.selectedPage.value,
+                onDestinationSelected: adminController.changePage,
+                labelType: NavigationRailLabelType.selected,
+                elevation: 4,
+                trailing: IconButton(
+                  icon: const Icon(Icons.logout)
+                  , onPressed: () {
                   Get.find<AuthController>().logout();
-              },
-              ),
-              destinations: const [
-                NavigationRailDestination(
-                  icon: Icon(Icons.add),
-                  label: Text('طلب دم'),
+                },
                 ),
-                NavigationRailDestination(
-                  icon: Icon(Icons.bloodtype_rounded),
-                  label: Text('طلبات الدم'),
-                ),
-                NavigationRailDestination(
-                  icon: Icon(Icons.people),
-                  label: Text('المتبرعين'),
-                ),
+                destinations: const [
+                  NavigationRailDestination(
+                    icon: Icon(Icons.add),
+                    label: Text('طلب دم'),
+                  ),
+                  NavigationRailDestination(
+                    icon: Icon(Icons.bloodtype_rounded),
+                    label: Text('طلبات الدم'),
+                  ),
+                  NavigationRailDestination(
+                    icon: Icon(Icons.people),
+                    label: Text('المتبرعين'),
+                  ),
 
-              ],
-            ),
-            Expanded(
-              child: Obx(() {
-                switch (adminController.selectedPage.value) {
-                  case 0:
-                    return BloodRequest();
-                  case 1:
-                    return DonationRequests();
-                  case 2:
-                    return DonorListScreen();
-                  default:
-                    return DonorListScreen();
-                }
-              }),
-            ),
-          ],
-        ),
+                ],
+              ),
+              Expanded(
+                child: Obx(() {
+                  switch (adminController.selectedPage.value) {
+                    case 0:
+                      return BloodRequest();
+                    case 1:
+                      return DonationRequests();
+                    case 2:
+                      return DonorListScreen();
+                    default:
+                      return DonorListScreen();
+                  }
+                }),
+              ),
+            ],
+          );
+        }),
       ),
     );
   }
